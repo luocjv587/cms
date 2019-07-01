52b295c5dd75cf1195cd6e75a5f59ac98eb6c0f6
chenjie.luo <547605677@qq.com>
2019-07-01 13:23:14 +0800

    3

--- app/Console/Commands/updateFormal.php
+++ app/Console/Commands/updateFormal.php
@@ -54,7 +54,8 @@ class updateFormal extends Command
 // 判断签名是否匹配
         if (strcmp($signature, $hash) == 0) {
             $cmd = "cd $target && git pull";
-            $res = shell_exec($cmd);
+//            $res = shell_exec($cmd);
+            $res = json_encode($this->doShell($cmd));
             $res_log = 'Success:' . PHP_EOL;
             $res_log .= $content['head_commit']['author']['name'] . ' 在' . date('Y-m-d H:i:s') . '向' . $content['repository']['name'] . '项目的' . $content['ref'] . '分支 push 了' . count($content['commits']) . '个 commit：' . PHP_EOL;
             $res_log .= $res . PHP_EOL;
@@ -67,5 +68,37 @@ class updateFormal extends Command
             $res_log .= '=======================================================================' . PHP_EOL;
             echo $res_log;
         }
+    }
+
+    /*
+ * 执行shell命令
+ */
+    protected function doShell($cmd, $cwd = null)
+    {
+        $descriptorspec = array(
+            0 => array("pipe", "r"), // stdin
+            1 => array("pipe", "w"), // stdout
+            2 => array("pipe", "w"), // stderr
+        );
+        $proc = proc_open($cmd, $descriptorspec, $pipes, $cwd, null);
+        // $proc为false，表明命令执行失败
+        if ($proc == false) {
+            return false;
+            // do sth with HTTP response
+            print_r("命令执行出错！");
+        } else {
+            $stdout = stream_get_contents($pipes[1]);
+            fclose($pipes[1]);
+            $stderr = stream_get_contents($pipes[2]);
+            fclose($pipes[2]);
+            $status = proc_close($proc); // 释放proc
+        }
+        $data = array(
+            'stdout' => $stdout, // 标准输出
+            'stderr' => $stderr, // 错误输出
+            'retval' => $status, // 返回值
+        );
+
+        return $data;
     }
 }
