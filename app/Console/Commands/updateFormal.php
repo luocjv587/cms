a4a512db16d37faaeb6d625adbef256f1220519a
chenjie.luo <547605677@qq.com>
2019-07-01 13:05:54 +0800

    111

--- app/Console/Commands/updateFormal.php
+++ app/Console/Commands/updateFormal.php
@@ -43,7 +43,6 @@ class updateFormal extends Command
 //获取 GitHub 发送的内容
 //        $json = file_get_contents('php://input');
 //        $content = json_decode($json, true);
-        $json = json_encode(request()->all());
         $content = request()->all();
 //github 发送过来的签名
         $signature = isset($_SERVER['HTTP_X_HUB_SIGNATURE']) ? $_SERVER['HTTP_X_HUB_SIGNATURE'] : '';
@@ -51,11 +50,9 @@ class updateFormal extends Command
             echo 'no signature';
             return http_response_code(404);
         }
-        list($algo, $hash) = explode('=', $signature, 2);
-//计算签名
-        $payloadHash = hash_hmac($algo, $json, $secret);
+        $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
 // 判断签名是否匹配
-        if ($hash === $payloadHash) {
+        if (strcmp($signature, $hash) == 0) {
             $cmd = "cd $target && git pull";
             $res = shell_exec($cmd);
             $res_log = 'Success:' . PHP_EOL;
